0b2531b77c231708a8e17bd28eee30bea21040fc
Connor Crawford <cdcraw@icloud.com>
2015-10-27 11:10:07 -0400

    Initial Commit

---
+++ Strings Library/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Strings Library
+//
+//  Created by Connor Crawford on 10/27/15.
+//  Copyright © 2015 Connor Crawford. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
